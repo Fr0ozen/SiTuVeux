a9ba1751b9b956e9cf52c4c06154416ad15c2a0f
Luc Rodiere <luc.rodiere@epsi.fr>
2019-01-14 11:29:50 +0100

    Le premier commit qui met tout en place

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { TestComponent } from './test/app.component';
+import { Test2Component } from './test2/app.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/test', pathMatch: 'full' },
+  { path: 'test', component: TestComponent },
+  { path: 'test2', component: Test2Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { TestComponent } from './test/app.component';
+import { Test2Component } from './test2/app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    TestComponent,
+    Test2Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [TestComponent, Test2Component]
 })
 export class AppModule { }
