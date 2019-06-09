cf4ec97878eef2ac2956296ba62192afe214e655
Nicolas DERAMBURE <nicolas.derambure@epsi.fr>
2019-06-09 18:12:04 +0200

    commit test

--- src/app/_components/encounter/encounter.component.ts
+++ src/app/_components/encounter/encounter.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit  } from '@angular/core';
 import { Team } from '../../_models/Team';
 import { PusherService } from '../pusher/pusher.service';
+import { Round } from 'src/app/_models/Round';
 
 
 @Component({
@@ -10,13 +11,9 @@ import { PusherService } from '../pusher/pusher.service';
 })
 
 export class EncounterComponent implements OnInit{
-
-    
-    title = 'Rencontre';
-    team1 = Teams[0];
-    team2 = Teams[1];
     @Input() team1Score : number;
     @Input() team2Score : number;
+    @Input() idMatch: number;
 
     constructor(private pusherService: PusherService){
         
@@ -37,10 +34,12 @@ export class EncounterComponent implements OnInit{
     gotRound(team: number){
         console.log("number:"+team.toString());
         if(team == 0){
+            const round = new Round(this.idMatch, 1, this.team1Score + this.team2Score + 1, new Date(Date.now()), new Date(Date.now()), true, true);
             this.team1Score ++;
             this.pusherService.like(0, this.team1Score);
         }
         else{
+            const round = new Round(this.idMatch, 1, this.team1Score + this.team2Score + 1, new Date(Date.now()), new Date(Date.now()), true, true);
             this.team2Score ++;
             this.pusherService.like(1, this.team2Score);
         }
---
+++ src/app/_models/Round.ts
@@ -0,0 +1,20 @@
+export class Round{
+    idmatch: number;
+    idteam: number;
+    roundnumber: number;
+    startingtime: Date;
+    endingtime: Date;
+    isct: boolean;
+    iswinner: boolean;
+
+    constructor(idmatch: number, idteam: number, roundnumber: number, startingtime: Date, endingtime: Date, isct: boolean, iswinner: boolean ){
+        this.idmatch = idmatch;
+        this.idteam = idteam;
+        this.roundnumber = roundnumber;
+        this.startingtime = startingtime;
+        this.endingtime = endingtime;
+        this.isct = isct;
+        this.iswinner = iswinner;
+    }
+}
+
